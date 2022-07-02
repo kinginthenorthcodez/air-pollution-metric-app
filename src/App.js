0cce515258bd9e52160b4b3414a22bdd173160cc
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-07-02 14:26:31 +0530

    Fix Routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './components/home';
 import Details from './components/details/Details';
-import store from './redux/configureStore';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:idCountry/:lat/:lng" element={<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:idCountry/:lat/:lng" element={<Details />} />
+      </Routes>
+    </>
   );
 }
 
